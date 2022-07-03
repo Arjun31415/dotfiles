98170b1bd73452f1c7a423916deeb977e32a9f13
arjun <arjunp710@gmail.com>
2022-07-03 15:09:19 +0530

    updated eww bar and dashboard

--- eww/.config/eww/scripts/dashActions.sh
+++ eww/.config/eww/scripts/dashActions.sh
@@ -15,68 +15,61 @@ _notify() {
 }
 
 pre_run() {
-	# Close eww dashboard
-	sh $HOME/.config/eww/scripts/openDashboard.sh
+    # Close eww dashboard
+    sh $HOME/.config/eww/scripts/openDashboard.sh
 }
 
 run_dnd() {
-	if [[ ! -f "$DND_LOCK_FILE" ]]; then
-		touch "$DND_LOCK_FILE"
-
-		dunstctl set-paused true
-	else
-		rm "$DND_LOCK_FILE"
-
-		dunstctl set-paused false
-	fi
+    if [[ ! -f "$DND_LOCK_FILE" ]]; then
+        touch "$DND_LOCK_FILE"
+        dunstctl set-paused true
+    else
+        rm "$DND_LOCK_FILE"
+        dunstctl set-paused false
+    fi
 }
 
 run_giph() {
-	if [[ ! -f "$JEFF_LOCK_FILE" ]]; then
-		touch "$JEFF_LOCK_FILE"
-
-		# Some stuff.
-		ps x | grep 'ffmpeg -f x11grab' | grep -v grep | awk '{print $1}' | xargs kill
-
-		sh $HOME/.local/bin/jeff selmp4
-	else
-		rm "$JEFF_LOCK_FILE"
-		ps x | grep 'ffmpeg -f x11grab' | grep -v grep | awk '{print $1}' | xargs kill -2
-	fi
+    if [[ ! -f "$JEFF_LOCK_FILE" ]]; then
+        touch "$JEFF_LOCK_FILE"
+        # Some stuff.
+        ps x | grep 'ffmpeg -f x11grab' | grep -v grep | awk '{print $1}' | xargs kill
+        sh $HOME/.local/bin/jeff selmp4
+    else
+        rm "$JEFF_LOCK_FILE"
+        ps x | grep 'ffmpeg -f x11grab' | grep -v grep | awk '{print $1}' | xargs kill -2
+    fi
 }
 
 is_giph_running() {
-	if [[ ! -f "$JEFF_LOCK_FILE" ]]; then
-		echo "$bgSecondary"
-	else
-		echo "#1c2325"
-	fi
+    if [[ ! -f "$JEFF_LOCK_FILE" ]]; then
+        echo 0
+    else
+        echo 1
+    fi
 }
 
 case $1 in
-	"dnd")
-		run_dnd
-		;;
-	"scrot")
-		# Yes, this is different from the sxhkdrc config.
-		pre_run
-
-		DATE=$(date '+%b%d-%H-%M.png')
-		maim -us | tee "$HOME/Pictures/Screenshots/$DATE" | xclip -selection clipboard -t image/png
-		;;
-	"jeff")
-		pre_run
-		
-		run_giph
-		;;
-	"dndstat")
-		if [[ ! -f "$DND_LOCK_FILE" ]]; then
-			echo "$bgSecondary"
-		else
-			echo "#1c2325"
-		fi
-		;;
-	"jstat")
-		is_giph_running
-		;;
+"dnd")
+    run_dnd
+    ;;
+"scrot")
+    pre_run
+    DATE=$(date '+%b%d-%H-%M.png')
+    maim -us | tee "$HOME/Pictures/Screenshots/$DATE" | xclip -selection clipboard -t image/png
+    ;;
+"jeff")
+    pre_run
+    run_giph
+    ;;
+"dndstat")
+    if [[ ! -f "$DND_LOCK_FILE" ]]; then
+        echo 0
+    else
+        echo 1
+    fi
+    ;;
+"jstat")
+    is_giph_running
+    ;;
 esac
--- eww/.config/eww/scripts/openDashboard.sh
+++ eww/.config/eww/scripts/openDashboard.sh
@@ -4,22 +4,21 @@ LOCK_FILE="$HOME/.cache/eww-dash.lock"
 EWW_BIN="/usr/bin/eww"
 
 run() {
-	${EWW_BIN} open dashboard
-	${EWW_BIN} update dash=true
+    ${EWW_BIN} open dashboard
+    ${EWW_BIN} update dash=true
 }
 
 # Run eww daemon if not running
-if [[ ! `pidof eww` ]]; then
-	${EWW_BIN} daemon
-	sleep 1
+if [[ ! $(pidof eww) ]]; then
+    ${EWW_BIN} daemon
+    sleep 1
 fi
-	if [[ ! -f "$LOCK_FILE" ]]; then
-		touch "$LOCK_FILE"
-		run
-	else
-		${EWW_BIN} update dash=false
-		sleep 0.4
-		${EWW_BIN} close dashboard
-		rm "$LOCK_FILE"
-	fi
+if [[ ! -f "$LOCK_FILE" ]]; then
+    touch "$LOCK_FILE"
+    run
+else
+    ${EWW_BIN} update dash=false
+    sleep 0.4
+    ${EWW_BIN} close dashboard
+    rm "$LOCK_FILE"
 fi
