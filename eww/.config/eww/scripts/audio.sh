ef7547478b2287b098d74e210a1bf42e2e885ac2
arjun <arjunp710@gmail.com>
2022-07-03 21:51:17 +0530

    updated eww dashboard

--- eww/.config/eww/scripts/audio.sh
+++ eww/.config/eww/scripts/audio.sh
@@ -11,17 +11,19 @@ case "$1" in
     done
     ;;
 "volume")
-    volume=$(pamixer --get-volume)
+    # volume=$(pamixer --get-volume)
+    volume=$(amixer sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }' | tr -d '%')
     echo $volume
     pactl subscribe |
         grep --line-buffered "Event 'change' on sink " |
         while read -r evt; do
             volume=$(pamixer --get-volume | cut -d " " -f1)
+
             echo $volume
         done
     ;;
 "toggle")
-    speaker_sink_id=$(pamixer --list-sinks  | grep "Komplete_Audio_6" | awk '{print $1}')
+    speaker_sink_id=$(pamixer --list-sinks | grep "Komplete_Audio_6" | awk '{print $1}')
     game_sink_id=$(pamixer --list-sinks | grep "stereo-game" | awk '{print $1}')
     case "$(pactl get-default-sink)" in
     *Arctis_9*)
